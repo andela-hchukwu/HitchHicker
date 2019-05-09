11f5fa97938eeb393864cd9a68e36d6d35d26cd3
Henry Chukwu <henry.chukwu@andela.com>
2019-05-09 16:07:55 +0100

    location services and permissions

--- HitchHicher/Controller/HomeViewController.swift
+++ HitchHicher/Controller/HomeViewController.swift
@@ -18,17 +18,45 @@ class HomeViewController: UIViewController {
 
     var delegate: CenterVCDelegate?
 
+    var manager: CLLocationManager?
+    var regionRadius: CLLocationDistance = 1000
+
     let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "launchScreenIcon")!, iconInitialSize: CGSize(width: 80, height: 80), backgroundColor: UIColor.white)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        manager = CLLocationManager()
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        checkLocationAuthStatus()
+
         mapView.delegate = self
+
+        centerMapOnUserLocation()
 
         self.view.addSubview(revealingSplashView)
         revealingSplashView.animationType = SplashAnimationType.heartBeat
         revealingSplashView.startAnimation()
 
         revealingSplashView.heartAttack = true
+    }
+
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            manager?.startUpdatingLocation()
+        } else {
+            manager?.requestAlwaysAuthorization()
+        }
+    }
+
+    func centerMapOnUserLocation() {
+        let coordinateRegion = MKCoordinateRegion(center: mapView.userLocation.coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
+    @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+        centerMapOnUserLocation()
     }
 
     @IBAction func requestRideBtnWasPressed(_ sender: Any) {
@@ -41,6 +69,18 @@ class HomeViewController: UIViewController {
 }
 
 extension HomeViewController: MKMapViewDelegate {
+
+}
+
+extension HomeViewController: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthStatus()
+        if status == .authorizedAlways {
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+        }
+    }
 
 }
 
